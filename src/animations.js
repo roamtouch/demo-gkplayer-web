d5f0259f908fe2fa6c34121f979c6d3c8dc7f08a
ivan-shulev <jonanishulev@gmail.com>
2014-07-31 20:53:15 +0300

    added touch animations

---
+++ src/animations.js
@@ -0,0 +1,45 @@
+function TouchAnimation(imagesSources, imgHolder){
+	this.imgHolder = imgHolder;
+	this.imagesSources = imagesSources;
+}
+
+TouchAnimation.prototype.generateImages = function(clientX, clientY){
+	var x = Math.floor((Math.random() * this.imagesSources.length));
+	var particleImg	= document.createElement("img");
+	particleImg.setAttribute('src', this.imagesSources[x]);
+	particleImg.className = 'particle-img';
+	particleImg.style.left = clientX + 'px';
+	particleImg.style.top = clientY + 'px';
+	this.imgHolder.appendChild(particleImg);
+}
+
+var removeImages;
+
+TouchAnimation.prototype.animateAndRemoveImages = function(recognizedGesture)
+{
+	clearTimeout(removeImages);
+	var particleImgs = document.getElementsByClassName('particle-img');
+	for(i=0; i<particleImgs.length; i++)
+	{
+		if(recognizedGesture)
+		{
+			particleImgs[i].style.paddingTop = Math.floor((Math.random() * 100) + 1 ) + 'px';
+			particleImgs[i].style.opacity = 0;
+		}
+		else
+		{
+			particleImgs[i].style.marginTop = Math.floor(Math.random() * 201) - 100 + 'px';
+			particleImgs[i].style.marginLeft = Math.floor(Math.random() * 201) - 100 + 'px';
+			particleImgs[i].style.opacity = 0;
+		}
+	}
+
+	var imgHolder = this.imgHolder;
+	removeImages = setTimeout(function()
+	{
+		while (imgHolder.firstChild) {
+			imgHolder.removeChild(imgHolder.firstChild);
+		}
+	},2000);
+}
+
